94b6618957c5e99881d6f042fbd5a95fd2c523ba
gulyasmir <gulyasmir@yandex.ru>
2021-12-02 12:34:53 +0300

    start work

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+import './App.css'
+import {Navbar} from "./components/Navbar"
+import {TodoForm} from "./components/TodoForm"
+import {TodoList} from "./components/TodoList"
+import {ITodo} from "./interfaces";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [todos, setTodos] = useState<ITodo[]>([])
+
+  const addHandler = (title:string) => {
+    const newTodo:ITodo = {
+      title: title,
+      id:Date.now(),
+      completed:false
+    }
+    setTodos(prev => [newTodo, ...prev])
+  }
+  const toggleHandler = (id:number) => {
+    setTodos(prev => prev.map(todo => {
+      if (todo.id === id) {
+        todo.completed = !todo.completed
+      }
+      return todo
+    }))
+  }
+
+  const removeHandler = (id:number) => {
+    const shouldRemove = window.confirm('Вы точно хотите удалить ?')
+    if (shouldRemove) {
+      setTodos(prev => prev.filter(todo => todo.id !== id))
+    }
+  }
+  return ( <>
+  <Navbar/>
+    <div className="container">
+      <h1>Test</h1>
+      <TodoForm
+        onAdd={addHandler}
+      />
+      <TodoList
+        todos={todos}
+        onToggle={toggleHandler}
+        onRemove={removeHandler}
+      />
     </div>
-  );
+  </>
+  )
 }
 
 export default App;
---
+++ src/components/TodoList.tsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import {ITodo} from '../interfaces'
+
+type TodoListProps = {
+    todos: ITodo[]
+    onToggle: (id:number) => void
+    onRemove: (id:number) => void
+}
+
+export const TodoList:React.FC<TodoListProps> = ({
+         todos,
+         onRemove,
+         onToggle
+    }) => {
+        if (todos.length === 0) {
+            return <p className="center">Ничего нет</p>
+    }
+
+    return(
+        <ul>
+            {todos.map(todo => {
+                let classes = ['todo']
+                if (todo.completed){
+                    classes.push('completed')
+                } else {
+                    classes = ['todo']
+                }
+
+                return (
+                    <li className={classes.join(' ')} key={todo.id}>
+                        <label>
+                            <input
+                                type="checkbox"
+                                checked={todo.completed}
+                                onChange={onToggle.bind(null, todo.id)}
+                            />
+                            <span>{todo.title}</span>
+                            <i
+                                className="material-icons  red-text"
+                                onClick={()=>onRemove(todo.id)}
+                            >delete</i>
+                        </label>
+                    </li>
+                )
+            })}
+
+        </ul>
+    )
+}
